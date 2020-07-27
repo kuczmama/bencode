7ac632ac0032826486441202d84969bbcc0f102b
Mark Kuczmarski <mark@mkucz.com>
2020-07-27 20:43:58 +0900

    Add test to make sure torrents load properly, and fixed bug with the number 9 not being parsed properly

--- test/index.test.js
+++ test/index.test.js
@@ -1,4 +1,4 @@
-const bencode = require("..");
+const bencode = require("../lib");
 const utils = require("./utils/utils");
 
 const test = (data, encoded) => {
@@ -9,15 +9,24 @@ const test = (data, encoded) => {
 
 console.log("Running tests...\n");
 
+// Numbers
 test(42, "i42e");
 test(0, "i0e");
 test(-42, "i-42e");
+test(9, "i9e");
+
+// String
 test("spam", "4:spam");
 test("abcdefghij", "10:abcdefghij");
+
+// Array
 test(["spam", 42], "l4:spami42ee");
 test({ bar: "spam", foo: 42 }, "d3:bar4:spam3:fooi42ee");
 
+// Dictionary
 test({ a: [1, 2, [3]], o: 1 });
+
+// Null Data
 test(null);
 test([[], [], [], [[], []]]);
 let data = {
@@ -33,5 +42,15 @@ utils.assert(
   bencode.encode(data),
   "d4:dictd3:key36:This is a string within a dictionarye7:integeri12345e4:listli1ei2ei3ei4e6:stringi5edee6:string11:Hello Worlde"
 );
+
+const fs = require("fs");
+const path = require("path");
+const torrent = fs.readFileSync(
+  path.resolve(__dirname, "./data/arch.iso.torrent")
+);
+
+const decodedTorrent = bencode.decode(torrent);
+utils.assert(decodedTorrent["url-list"].length, 310);
+utils.assert(decodedTorrent.info.name, "archlinux-2020.07.01-x86_64.iso");
 
 utils.printTestResults();
