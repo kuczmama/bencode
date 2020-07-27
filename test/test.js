0cd9adc69f888dcd769120783876ceb855bd2fc7
Mark Kuczmarski <mark@mkucz.com>
2020-07-27 20:19:44 +0900

    Add colors to test runner

--- test/test.js
+++ test/test.js
@@ -1,30 +1,37 @@
-const bencode = require('..');
-const utils = require('./utils/utils');
+const bencode = require("..");
+const utils = require("./utils/utils");
 
 const test = (data, encoded) => {
-    if(encoded == null) encoded = bencode.encode(data);
-    utils.assert(bencode.encode(data), encoded);
-    utils.assert(bencode.decode(encoded), data);
-}
-
-test(42, 'i42e');
-test(0, 'i0e');
-test(-42, 'i-42e');
-test('spam', '4:spam');
-test('abcdefghij', '10:abcdefghij');
-test(['spam', 42], 'l4:spami42ee');
-test({bar: 'spam', foo: 42}, 'd3:bar4:spam3:fooi42ee');
-
-test({a: [1,2,[3]], o: 1});
-test(null);
-test([[],[],[],[[],[]]]);
-let data = {
-    string: 'Hello World',
-    integer: 12345,
-    dict: {
-      key: 'This is a string within a dictionary'
-    },
-    list: [ 1, 2, 3, 4, 'string', 5, {} ]
+  if (encoded == null) encoded = bencode.encode(data);
+  utils.assert(bencode.encode(data), encoded);
+  utils.assert(bencode.decode(encoded), data);
 };
 
-utils.assert(bencode.encode(data), 'd4:dictd3:key36:This is a string within a dictionarye7:integeri12345e4:listli1ei2ei3ei4e6:stringi5edee6:string11:Hello Worlde')
+console.log("Running tests...\n");
+
+test(42, "i42e");
+test(0, "i0e");
+test(-42, "i-42e");
+test("spam", "4:spam");
+test("abcdefghij", "10:abcdefghij");
+test(["spam", 42], "l4:spami42ee");
+test({ bar: "spam", foo: 42 }, "d3:bar4:spam3:fooi42ee");
+
+test({ a: [1, 2, [3]], o: 1 });
+test(null);
+test([[], [], [], [[], []]]);
+let data = {
+  string: "Hello World",
+  integer: 12345,
+  dict: {
+    key: "This is a string within a dictionary",
+  },
+  list: [1, 2, 3, 4, "string", 5, {}],
+};
+
+utils.assert(
+  bencode.encode(data),
+  "d4:dictd3:key36:This is a string within a dictionarye7:integeri12345e4:listli1ei2ei3ei4e6:stringi5edee6:string11:Hello Worlde"
+);
+
+utils.printTestResults();
--- test/utils/utils.js
+++ test/utils/utils.js
@@ -1,22 +1,47 @@
 isEqual = (expected, actual) => {
-    if(expected === actual) return true;
-    if(typeof expected !== typeof actual) return false;
-    if(Array.isArray(expected) && Array.isArray(actual)) {
-        if(expected.length !== actual.length) return false;
-        let sol = true;
-        for(let i = 0; i < expected.length; i++) {
-            sol = sol && isEqual(expected[i], actual[i]);
-        }
-        return sol;
+  if (expected === actual) return true;
+  if (typeof expected !== typeof actual) return false;
+  if (Array.isArray(expected) && Array.isArray(actual)) {
+    if (expected.length !== actual.length) return false;
+    let sol = true;
+    for (let i = 0; i < expected.length; i++) {
+      sol = sol && isEqual(expected[i], actual[i]);
     }
-    return JSON.stringify(expected) === JSON.stringify(actual);
-}
+    return sol;
+  }
+  return JSON.stringify(expected) === JSON.stringify(actual);
+};
 
+const red = (str) => `\x1b[31m${str}\x1b[0m`;
+const green = (str) => `\x1b[32m${str}\x1b[0m`;
 
+let testNum = 0;
+const passed = [];
+const failed = [];
 module.exports.assert = (actual, expected) => {
-    if(isEqual(expected, actual)) {
-        console.log(".");
-    } else {
-        console.log(`Expected: ${JSON.stringify(expected)}, but got: ${JSON.stringify(actual)}`);
-    }
-}
+  testNum++;
+  if (isEqual(expected, actual)) {
+    process.stdout.write(green("."));
+    passed.push(".");
+  } else {
+    const message = red(
+      `${testNum} Expected: ${JSON.stringify(
+        expected
+      )}, but got: ${JSON.stringify(actual)}`
+    );
+    failed.push(message);
+    console.log(message);
+  }
+};
+
+module.exports.printTestResults = () => {
+  if (failed.length === 0) {
+    console.log(`\n\nAll ${passed.length} tests passed`);
+  } else {
+    console.log(
+      `\n\n${red(`Failed: ${failed.length} Tests`)}\nRan ${
+        passed.length + failed.length
+      } Tests\nPassed: ${passed.length}`
+    );
+  }
+};
